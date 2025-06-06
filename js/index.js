b751225f54f6375de57f6a49dc5f60f6bff8cab4
KÉPLER <tandat6444@gmail.com>
2025-06-06 20:28:07 +0700

    js4

--- js/index.js
+++ js/index.js
@@ -6,17 +6,6 @@ let tinhTongDiem = function (arrInput) {
     }
     return tongDiem;
 }
-// Check điểm hợp lệ đầu vào
-let checkHopLe = function (arrDiem) {
-    for (let j in arrDiem) {
-        let hopLe = 1;
-        if (arrDiem[j] < 0 || arrDiem[j] > 10) {
-            hopLe = 0;
-        }
-    }
-    return hopLe;
-}
-
 // Bài 1
 document.querySelector('#quanLyTuyenSinh').onsubmit = function (event) {
     event.preventDefault();
@@ -26,7 +15,6 @@ document.querySelector('#quanLyTuyenSinh').onsubmit = function (event) {
     let ketQua = '';
 
     if (diemChuan > 0 && diemChuan <= 30) {
-        console.log('Điểm chuẩn hợp lệ');
 
         let arrInput = document.querySelectorAll('.bai1-input'); //DOM các đầu vào (select, input)
 
@@ -36,9 +24,10 @@ document.querySelector('#quanLyTuyenSinh').onsubmit = function (event) {
                 arrInfo.push(+i.value); // Mặc định thêm value của select vào chuỗi
             } else if (i.tagName == "INPUT") {
 
-                if (+i.value > 0) {     // Kiểm tra số 0 trong chuỗi các input
+
+
+                if (+i.value > 0 && +i.value <= 10) {     // Kiểm tra số 0 trong chuỗi các input
                     arrInfo.push(+i.value);
-                    console.log(arrInfo);
 
                     // Tính tổng điểm
                     let diemTong = tinhTongDiem(arrInfo);
@@ -49,7 +38,7 @@ document.querySelector('#quanLyTuyenSinh').onsubmit = function (event) {
                     }
 
                 } else {
-                    ketQua = `Thất bại do có điểm <strong>0</strong>.`;
+                    ketQua = `Thất bại do có điểm <strong>0</strong> hoặc điểm nhập vào không hợp lệ.`;
                     break;
                 }
             }
@@ -85,7 +74,6 @@ let tinhTienDien = function (input) {
     }
     return money;
 }
-console.log(tinhTienDien(351));
 
 document.querySelector('#tinhTienDien').onsubmit = function (event) {
     event.preventDefault();
@@ -97,4 +85,89 @@ document.querySelector('#tinhTienDien').onsubmit = function (event) {
     let soTien = tinhTienDien(suDung);
 
     document.querySelector('#ketQua2').innerHTML = `Tên: <strong>${name}</strong> - Tiền điện: <strong>${soTien}</strong>.`;
+}
+
+// ----------- Bài 3 --------------------
+let tinhThue = function (input, minus) {
+    let money = input * 0.1 - minus * 160000;
+    return money;
+}
+
+document.querySelector('#tinhTienThue').onsubmit = function (event) {
+    event.preventDefault();
+    let thongTin = document.querySelector('#caNhan').value;
+    let ketQua3 = '';
+
+
+    let thuNhap = +document.querySelector('#thuNhap').value;
+    let phuThuoc = +document.querySelector('#phuThuoc').value;
+    if (thuNhap > 4000000) {
+        // Tính thuế
+        let tienThue = tinhThue(thuNhap, phuThuoc);
+        ketQua3 = `Tên: <strong>${thongTin}</strong>, Tiền thuế cá nhân: <strong>${tienThue}</strong>.`
+    } else {
+        ketQua3 = 'Thông tin nhập chưa đúng.'
+    }
+
+    document.querySelector('#ketQua3').innerHTML = ketQua3;
+}
+// ---------- Bài 4 ---------------
+
+// Function Onchange
+let themKetNoi = function () {
+    let loaiKH = +document.querySelector('#loaiKH').value;
+    console.log(loaiKH);
+    if (loaiKH == 2) {
+        document.querySelector('#soKetNoi').className = 'form-control';
+    } else {
+        document.querySelector('#soKetNoi').className = 'form-control d-none';
+    }
+}
+
+let bonusDN = function (soKetNoi) {
+    let bonus = 75;
+
+    if (soKetNoi <= 10) {
+        bonus += 0;
+    } else {
+        bonus += (soKetNoi - 10) * 5;
+    }
+
+    return bonus;
+}
+
+let tinhTienCap = function (soKenh, heSo) {
+    let money = 25;
+    money += soKenh * (7.5 + heSo); // Dân: (25 * 1) + soKenh * (7.5);
+    return money;
+}
+
+document.querySelector('#tinhTienCap').onsubmit = function (event) {
+    event.preventDefault();
+    let soTien = 0;
+    let maKH = document.querySelector('#maKH').value;
+
+    let loaiKH = +document.querySelector('#loaiKH').value;
+    let kenhCaoCap = +document.querySelector('#kenhCaoCap').value;
+    let ketNoi = +document.querySelector('#soKetNoi').value;
+
+    console.log(loaiKH);
+    console.log(kenhCaoCap);
+
+
+    switch (loaiKH) {
+        case 1: {
+            soTien += tinhTienCap(kenhCaoCap, 0);
+            break;
+        }
+        case 2: {
+            soTien += tinhTienCap(kenhCaoCap, 42.5) + bonusDN(ketNoi);
+            break;
+        } default: {
+            window.alert('Vui lòng chọn loại khách hàng');
+            break;
+        }
+    }
+
+    document.querySelector('#ketQua4').innerHTML = `Mã khách hàng: <strong>${maKH}</strong> - Tiền cáp: <strong>${soTien}</strong>.`;
 }
